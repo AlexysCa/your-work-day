acf32662f6ddc2265b77f377eedf1ecca776ca7f
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-03 20:46:04 -0500

    creating time variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,20 @@
-// variables here
+// variables here for moment
+
+// other variables 
+var eightAm = $("#8am");
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#1pm");
+var twoPm = $("#2pm");
+var threePm = $("#3pm");
+var fourPm = $("#4pm");
+var fivePm = $("#5pm");
+var sixPm = $("#6pm");
+
+
+// saving text events here
+function saveEvent() {
+
+}
